e8f2d5154d66238d006d53e1545273cd82d576cf
kreesh123 <kireetichakilam@gmail.com>
2026-07-15 16:04:42 -0400

    Fixing Windows Audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 
 import 'screens/eeg_viewer_screen.dart';
 import 'screens/user_window.dart';
@@ -6,16 +7,16 @@ import 'screens/user_window.dart';
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
 
+  JustAudioMediaKit.ensureInitialized(
+    windows: true,
+    linux: false,
+  );
+
   if (args.isNotEmpty && args.first == 'multi_window') {
     final windowId = int.parse(args[1]);
     final argument = args.length > 2 ? args[2] : '{}';
 
-    runApp(
-      UserWindowApp(
-        windowId: windowId,
-        argument: argument,
-      ),
-    );
+    runApp(UserWindowApp(windowId: windowId, argument: argument));
   } else {
     runApp(const MainApp());
   }
